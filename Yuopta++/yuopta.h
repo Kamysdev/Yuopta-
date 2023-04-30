bf79aaf056fa82f732e842e27de8b65b664836e7
Kamys <sereginka070504@gmail.com>
2023-05-01 01:16:38 +0700

    Yuopta++ ver 0.1

---
+++ Yuopta++/yuopta.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#define залупа std
+#define начусл {
+#define конусл }
+#define сука_текст_кончился endl
+#define двенашка new
+
+// Data
+#define число int
+#define WorldOfWarships float
+#define базарю true
+#define пиздёж false
+
+#define школота class
+
+
+#define база main
+
+// Logic
+#define жигули for
+#define абажыбля while
+
+#define жиесть if
+#define естьжи else
+
+
+// Operators
+#define бочёкпотик break
+#define накончать return
+#define граффити cout
+#define запомните_твари cin
